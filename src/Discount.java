b80cb8f34844c525514a240094d02b47a11b5301
SaPalpita <sanjikaashpalpita@gmail.com>
2022-07-21 08:20:17 +0530

    Adding chain

---
+++ src/Discount.java
@@ -0,0 +1,16 @@
+public class Discount extends BillHandler{
+
+    @Override
+    public double applyDeduction(Bill bill) {
+        bill.setDeduction(bill.getAmmount() - bill.getAmmount()*0.1);
+        System.out.println("Discount Calculated");
+
+        if(bill.getAmmount()<=1000){
+            return bill.getAmmount();
+        }
+        else{
+            return successor.applyDeduction(bill);
+        }
+    }
+
+}
